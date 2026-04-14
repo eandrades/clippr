fb8c935130ea19682ffd574e3af33467dc7908b6
eandrades <eder.jam@gmail.com>
2026-04-14 01:06:45 -0400

    feat: mobile responsive + viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
